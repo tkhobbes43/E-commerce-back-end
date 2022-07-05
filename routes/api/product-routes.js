bacb8007d67d41828eb22e5a4dd809e18cd586f4
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-05 00:14:58 -0500

    get one product a success on insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,9 +27,34 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      ]
+    });
+    
+    if (!productData) {
+      res.status(404).json({ message: 'Product is not found!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
