79250a1e497c4637826bd876c169e9d1fbc4ed3b
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-05 23:36:08 -0500

    view all tags successful

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,22 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
