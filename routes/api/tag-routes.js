74f225f260f6d42956afae44dff282736acc597f
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-06 00:11:36 -0500

    create a new tag and update tag by its id successful

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,12 +47,35 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (error) {
+      res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+
+    if(!tagData[0]) {
+      res.status(404).json( {message: 'No tag found with this id!'} );
+      return;
+    } res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
